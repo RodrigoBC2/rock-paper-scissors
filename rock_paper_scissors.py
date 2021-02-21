dcaa2c23c995a2052ca4b4e2ae3fd6d16072cff0
Rodrigo Bessa <56771198+RodrigoBC2@users.noreply.github.com>
2021-02-21 01:13:16 -0300

    Update rock_paper_scissors.py
    additional feature implemented: while loop on the beginning to reload the game and a while loop in the end to manage if the user presse the correct button on the keyboard.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,46 +1,71 @@
 #library that generates random numbers
 from random import randint
 
-# dictionary populated with options
-opt_dict = {1 : 'ROCK', 2 : 'PAPER', 3 : 'SCISSORS'}
+reboot = True
 
-print('Choose one of the options below:')
-print('Option 1 = Rock.\nOption 2 = Paper.\nOption 3 = Scissors.')
+#loop to reload the game
+while reboot == True:
+    
+    # dictionary populated with options
+    opt_dict = {1 : 'ROCK', 2 : 'PAPER', 3 : 'SCISSORS'}
+    
+    print('Choose one of the options below:')
+    print('Option 1 = ROCK.\nOption 2 = PAPER.\nOption 3 = SCISSORS.')
+    
+    user = int(input())
+    
+    # generating an integer random number and saving it in a variable
+    rand = randint(1,3)
 
-user = int(input())
+    pc = opt_dict.get(rand)
+    
+    # checking if the duel is a draw.
+    if user == pc:
+        print('DRAW!')
+        
+    # if its not a DRAW, than the program will check other options
+    elif user == 1:
+        print('User choose: ROCK.')
+        print('Computer choose: ', pc)
+        
+        if pc == opt_dict.get(2):
+            print('Computer has won!')
+        
+        elif pc == opt_dict.get(3):
+            print('User has won!')
+    
+    elif user == 2:
+        print('User choose: PAPER.')
+        print('Computer choose: ', pc)
+        
+        if pc == opt_dict.get(3):
+            print('Computer has won!')
+        
+        elif pc == opt_dict.get(1):
+            print('User has won!')
+        
+    elif user == 3:
+        print('User choose: SCISSORS.')
+        print('Computer choose: ', pc)
+        
+        if pc == opt_dict.get(1):
+            print('Computer has won!')
+        
+        elif pc == opt_dict.get(2):
+            print('User has won!')
+    
+    # user input choosing play again, "yes" and "not" notation.
+    choice = input("\nDo you want to play again? press y if yes or n if not")
+    
+    # loop created to avoid any other input than yes or not keys
+    while choice != 'y' and choice != 'n':
+        
+        choice = input("\nDo you want to play again? press y if yes or n if not\n")
+    
+    # if yes, the initial loop still active    
+    if choice == 'y':
+        reboot = True
 
-# generating an integer random number and saving it in a variable
-rand = randint(1,3)
-pc = opt_dict.get(rand)
-
-# checking if the duel is a draw.
-if user == pc:
-    print('DRAW!')
-
-# if its not a DRAW, than the program will check other options
-elif user == 1:
-    print('User choose: ROCK.')
-    print('Computer choose: ', pc)
-
-    if pc == opt_dict.get(2):
-        print('Computer has won!')
-    elif pc == opt_dict.get(3):
-        print('User has won!')
-
-if user == 2:
-    print('User choose: PAPER.')
-    print('Computer choose: ', pc)
-
-    if pc == opt_dict.get(3):
-        print('Computer has won!')
-    elif pc == opt_dict.get(1):
-        print('User has won!')
-
-if user == 3:
-    print('User choose: SCISSORS.')
-    print('Computer choose: ', pc)
-
-    if pc == opt_dict.get(1):
-        print('Computer has won!')
-    elif pc == opt_dict.get(2):
-        print('User has won!')
+    # if not, the initial variable will kill the while loop, finishing the program.    
+    elif choice == 'n':
+        reboot = False
