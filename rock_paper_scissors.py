242afc32e259704f5ca4edf5a9bb5071ca3c7519
Rodrigo Bessa <56771198+RodrigoBC2@users.noreply.github.com>
2021-02-21 18:38:03 -0300

    Update rock_paper_scissors.py
    The major change on this version is merging all conditions to a function and return with the result of the game.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,67 +1,63 @@
-#library that generates random numbers
+# library to generate "random" numbers
 from random import randint
+
+# library to substitue all if and elif from the first version of the code
+def conditionals(x, y):
+
+    # verifying if the game is a draw
+    if x == y:
+        winner = 'A Draw!'
+        return winner
+
+    # shrink all user possibilities to win in a single condition
+    elif x == 1 and y != 2 or x == 2 and y != 3 or x == 3 and y != 1:
+    
+        winner = 'User has won!'
+        return winner
+
+    # if none of coditions above is satisfied, then the computer wins
+    else:
+                   
+        winner = 'Computer has won!'
+        return winner
 
 reboot = True
 
-#loop to reload the game
+# loop to reload the game
 while reboot == True:
-    
+        
     # dictionary populated with options
     opt_dict = {1 : 'ROCK', 2 : 'PAPER', 3 : 'SCISSORS'}
-    
+
     print('Choose one of the options below:')
     print('Option 1 = ROCK.\nOption 2 = PAPER.\nOption 3 = SCISSORS.')
-    
+        
+    # inputs from user and computer
     user = int(input())
-    
-    # generating an integer random number and saving it in a variable
     rand = randint(1,3)
 
-    pc = opt_dict.get(rand)
-    
-    # checking if the duel is a draw.
-    if opt_dict.get(user) == pc:
-        print('\n It is a DRAW!')
-        
-    # if its not a DRAW, than the program will check other options
-    elif user == 1:
-        print('User choose: ROCK.')
-        print('Computer choose: ', pc)
-        
-        if pc == opt_dict.get(2):
-            print('\nComputer has won!')
-        
-        elif pc == opt_dict.get(3):
-            print('\nUser has won!')
-    
-    elif user == 2:
-        print('User choose: PAPER.')
-        print('Computer choose: ', pc)
-        
-        if pc == opt_dict.get(3):
-            print('\nComputer has won!')
-        
-        elif pc == opt_dict.get(1):
-            print('\nUser has won!')
-        
-    elif user == 3:
-        print('User choose: SCISSORS.')
-        print('Computer choose: ', pc)
-        
-        if pc == opt_dict.get(1):
-            print('\nComputer has won!')
-        
-        elif pc == opt_dict.get(2):
-            print('\nUser has won!')
-    
+    # variables given to "conditionals" function
+    x = user
+    y = rand
+
+    # checking the choice of the players
+    print("The user choice is: ", opt_dict.get(user))
+    print("The computer choice is: ", opt_dict.get(rand))  
+
+    # variable that obtains the result given by "conditionals" function
+    game_result = conditionals(x,y)
+
+    # printing the result on screen
+    print("The result is: ", game_result)
+
     # user input choosing play again, "yes" and "not" notation.
     choice = input("\nDo you want to play again? press y if yes or n if not")
     
     # loop created to avoid any other input than yes or not keys
     while choice != 'y' and choice != 'n':
-        
+            
         choice = input("\nDo you want to play again? press y if yes or n if not\n")
-    
+      
     # if yes, the initial loop still active    
     if choice == 'y':
         reboot = True
